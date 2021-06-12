e1192c055f11724860625b9b4de707abea4baedf
RotonEvan <debajyotih@iitbhilai.ac.in>
2021-06-13 03:51:38 +0530

    github repo link added

--- fybrrLink_VizTool/settings.py
+++ fybrrLink_VizTool/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+from VizTool.code.bresenham import F
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
